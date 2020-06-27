c3818fdccea7b5a719cb9d4948666ca7afb63001
Gio <giortiz@live.com>
2020-06-27 16:05:31 -0500

    ejercicio 1

---
+++ me.js
@@ -0,0 +1,10 @@
+var teacher = "Giovanni";
+var twitterHandle = "giortix";
+var age = 24;
+function whoAmI(myName, myNickName, myAge){
+    console.log(`
+        Hi, I´m ${myName}(aka ${myNickName}),
+        and I´m ${myAge} years old.
+        `);
+}
+whoAmI(teacher,twitterHandle,age);
